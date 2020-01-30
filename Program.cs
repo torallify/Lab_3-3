a0000f06cc3eb61a8ea14865861fba83b490d4c0
Lucero Toral <lucerotoral@quickenloans.com>
2020-01-30 16:22:35 -0500

    updating code to accept sentences

--- Program.cs
+++ Program.cs
@@ -11,10 +11,29 @@ namespace Lab_3_3
         {
             Console.Write("Please enter a word you would like to reverse: ");
             string userInput = Console.ReadLine();
+            
 
-            string newWord = ReverseString(userInput);
+            if (userInput.Contains(" "))
+            {
+                var sentence = userInput.Split(" ");
+                string newSentence = null;
 
-            Console.WriteLine($"Your word in reverse: {newWord}");
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    newSentence += ReverseString(sentence[i]) + " ";
+                }
+
+                Console.WriteLine($"You entered a sentence and your new sentence is: {newSentence}");
+
+            }
+            else
+            {
+                string newWord = ReverseString(userInput);
+
+                Console.WriteLine($"Your word in reverse: {newWord}");
+            }
+
+          
         }
 
         static string ReverseString(string word)
